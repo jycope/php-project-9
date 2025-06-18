2e970db38d7107be31527fea7a9f9a5c2645e0f0
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2025-06-18 13:14:45 +0500

    реализация шаблонов

--- public/index.php
+++ public/index.php
@@ -1,14 +1,31 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set(
+  'renderer',
+  function () {
+    $renderer = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+    $renderer->setLayout('base.phtml');
+
+    return $renderer;
+  }
+);
+
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
-  $response->getBody()->write('Welcome to Slim!');
-  return $response;
-});
+$app->get(
+  '/',
+  function ($request, $response) {
+    $params = ['message' => 'Dmitry'];
+
+    return $this->get('renderer')->render($response, 'index.phtml', $params);
+  }
+);
+
 $app->run();
